63ae4fec3bb43471f32e3ec5402fa863362fe632
parklot <zixingyx@gmail.com>
2022-02-24 17:16:16 +0800

    first commit

---
+++ src/main/java/com/objectsky/objectskywikiprogram/ObjectSkyWikiProgramApplication.java
@@ -0,0 +1,13 @@
+package com.objectsky.objectskywikiprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectSkyWikiProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectSkyWikiProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/objectsky/objectskywikiprogram/ObjectSkyWikiProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectsky.objectskywikiprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectSkyWikiProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
