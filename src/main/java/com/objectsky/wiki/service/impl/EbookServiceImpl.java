87f80896726a8aa684d8e716f1fba6a622dbc190
parklot <zixingyx@gmail.com>
2022-02-25 23:54:44 +0800

    封装了CopyUtils

--- src/main/java/com/objectsky/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/com/objectsky/wiki/service/impl/EbookServiceImpl.java
@@ -1,17 +1,16 @@
 package com.objectsky.wiki.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.objectsky.wiki.common.dto.EbookDto;
+import com.objectsky.wiki.common.utils.CopyUtil;
 import com.objectsky.wiki.common.vo.EbookVo;
 import com.objectsky.wiki.entity.Ebook;
 import com.objectsky.wiki.mapper.EbookMapper;
 import com.objectsky.wiki.service.EbookService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,14 +32,18 @@ public class EbookServiceImpl extends ServiceImpl<EbookMapper, Ebook> implements
         wrapper.like("name", ebookDto.getName());
 
         List<Ebook> ebooksListDb = ebookMapper.selectList(wrapper);
-        List<EbookVo> ebookList = new ArrayList<>();
-        for (Ebook ebook : ebooksListDb) {
-            EbookVo ebookVo = new EbookVo();
-            // spring tools
-            BeanUtils.copyProperties(ebook, ebookVo);
-            ebookList.add(ebookVo);
-        }
 
-        return ebookList;
+//        List<EbookVo> ebookList = new ArrayList<>();
+//        for (Ebook ebook : ebooksListDb) {
+//            // EbookVo ebookVo = new EbookVo();
+//            // BeanUtils.copyProperties(ebook, ebookVo);
+//
+//            EbookVo ebookVo = CopyUtil.copy(ebook, EbookVo.class);
+//            ebookList.add(ebookVo);
+//        }
+
+        List<EbookVo> ebookVoList = CopyUtil.copyList(ebooksListDb, EbookVo.class);
+
+        return ebookVoList;
     }
 }
