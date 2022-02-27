16363eede8bd75d122e0dbd6bf1f7a13f868187f
parklot <zixingyx@gmail.com>
2022-02-27 14:45:14 +0800

    新增cors跨域处理配置文件

---
+++ src/main/java/com/objectsky/wiki/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.objectsky.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Classname: CorsConfig
+ * @Description:
+ * @Date: Create in 2:40 PM 2022/2/27
+ * @Created By: JinleWeng
+ * @Modified By:
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
+
