fb24c1982d0fe9dcf5307d49e0331c452606e97b
parklot <zixingyx@gmail.com>
2022-02-27 17:42:21 +0800

    增加aop 用来打印接口耗时 去掉过滤器、拦截器

--- src/main/java/com/objectsky/wiki/interceptor/LogInterceptor.java
+++ src/main/java/com/objectsky/wiki/interceptor/LogInterceptor.java
@@ -1,45 +1,45 @@
-package com.objectsky.wiki.interceptor;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * @Classname: LogInterceptor
- * @Description: 用来打印请求耗时的拦截器
- * @Date: Create in 5:22 PM 2022/2/27
- * @Created By: JinleWeng
- * @Modified By:
- */
-
-/**
-  * 拦截器：Spring框架特有的，常用于登录校验，权限校验，请求日志打印 /login
-  */
- @Component
- public class LogInterceptor implements HandlerInterceptor {
-
-     private static final Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
-
-     @Override
-     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-         // 打印请求信息
-         LOG.info("------------- LogInterceptor 开始 -------------");
-         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("远程地址: {}", request.getRemoteAddr());
-
-         long startTime = System.currentTimeMillis();
-         request.setAttribute("requestStartTime", startTime);
-         return true;
-     }
-
-     @Override
-     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-         long startTime = (Long) request.getAttribute("requestStartTime");
-         LOG.info("------------- LogInterceptor 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+//package com.objectsky.wiki.interceptor;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * @Classname: LogInterceptor
+// * @Description: 用来打印请求耗时的拦截器 aop的比较好 所以注释掉拦截器的
+// * @Date: Create in 5:22 PM 2022/2/27
+// * @Created By: JinleWeng
+// * @Modified By:
+// */
+//
+///**
+//  * 拦截器：Spring框架特有的，常用于登录校验，权限校验，请求日志打印 /login
+//  */
+// @Component
+// public class LogInterceptor implements HandlerInterceptor {
+//
+//     private static final Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
+//
+//     @Override
+//     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//         // 打印请求信息
+//         LOG.info("------------- LogInterceptor 开始 -------------");
+//         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//         LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//         long startTime = System.currentTimeMillis();
+//         request.setAttribute("requestStartTime", startTime);
+//         return true;
+//     }
+//
+//     @Override
+//     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//         long startTime = (Long) request.getAttribute("requestStartTime");
+//         LOG.info("------------- LogInterceptor 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//     }
+// }
