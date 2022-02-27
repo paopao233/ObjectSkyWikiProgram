be77ab2f149606d3de338d3859093e8c011fa3b9
parklot <zixingyx@gmail.com>
2022-02-27 17:31:03 +0800

    增加拦截器 用来打印接口耗时

---
+++ src/main/java/com/objectsky/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.objectsky.wiki.config;
+
+import com.objectsky.wiki.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+
+    }
+}
