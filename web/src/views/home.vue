dd0ce81a4d9896a3adda58b547a4de800103d92a
parklot <zixingyx@gmail.com>
2022-02-28 11:31:13 +0800

    修改电子书列表页面

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -81,17 +81,6 @@ import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
 
 const listData: Record<string, string>[] = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
 
 export default defineComponent({
   components: {
